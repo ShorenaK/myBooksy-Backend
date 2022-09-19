845313d2552ee9c8239a49a33477eb8b770005bd
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-09-19 10:40:18 -0400

    data point added to mongoDB

--- models/books.js
+++ models/books.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
 
 const bookSchema = new mongoose.Schema({
     title: {type: String, required: true},
@@ -12,6 +14,17 @@ const bookSchema = new mongoose.Schema({
     likes: {type: Number}
 })
 
-const book = mongoose.model('book', bookSchema)
+const Book = mongoose.model('Book', bookSchema)
 
-module.exports = book
+module.exports = Book
+
+Book.create({
+    title: 'The Self-Taught Progammer: The Definitive Guide to Programming Professionally',
+    image: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/31YJmObNTnL._SX404_BO1,204,203,200_.jpg',
+    author: 'Cory Althoff',
+    genre: 'Non-fiction',
+    pages: 299,
+    description: "I am a self-taught programmer. After a year of self-study, I learned to program well enough to land a job as a software engineer II at eBay. Once I got there, I realized I was severely under-prepared. I was overwhelmed by the amount of things I needed to know but hadn't learned yet. My journey learning to program, and my experience at my first job as a software engineer were the inspiration for this book.",
+    publishDate: Date(2017, 0, 24),
+    likes: 0
+})
--- models/reviews.js
+++ models/reviews.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoURI = process.env.MONGODB_URI;
+mongoose.connect(mongoURI)
 
 const reviewSchema = new mongoose.Schema({
     recommend: {type: Boolean, required: true },
     comment: {type: String},
-    book: {type: mongoose.Types.ObjectId, ref: "book", required: true}
-})
+    book: {type: mongoose.Types.ObjectId, ref: "Book", required: true}
+},{timestamps: true})
+
+const Review = mongoose.model('Review', reviewSchema)
+
+module.exports = Review
+
