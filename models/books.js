4bb2186a6070ecaaa1b8839c37c82650d56ce620
ShahryarAbbasi <shizzleabbasi@gmail.com>
2022-09-19 11:02:44 -0400

    added more books and fixed date

--- models/books.js
+++ models/books.js
@@ -9,7 +9,7 @@ const bookSchema = new mongoose.Schema({
     author: {type: String, required: true},
     genre: {type: String, required: true},
     pages: {type: Number, required: true},
-    description: {type: String, required: true},
+    description: {type: String},
     publishDate: {type: Date, required: true},
     likes: {type: Number}
 })
@@ -19,12 +19,12 @@ const Book = mongoose.model('Book', bookSchema)
 module.exports = Book
 
 Book.create({
-    title: 'The Self-Taught Progammer: The Definitive Guide to Programming Professionally',
-    image: 'https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/31YJmObNTnL._SX404_BO1,204,203,200_.jpg',
-    author: 'Cory Althoff',
+    title: 'Conversations with Friends',
+    image: 'https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B600%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9780451499066_p0_v9_s600x595.jpg%5D&scale=options%5Blimit%5D,size%5B600x10000%5D&sink=format%5Bwebp%5D',
+    author: 'Sally Rooney',
     genre: 'Non-fiction',
-    pages: 299,
-    description: "I am a self-taught programmer. After a year of self-study, I learned to program well enough to land a job as a software engineer II at eBay. Once I got there, I realized I was severely under-prepared. I was overwhelmed by the amount of things I needed to know but hadn't learned yet. My journey learning to program, and my experience at my first job as a software engineer were the inspiration for this book.",
-    publishDate: Date(2017, 0, 24),
+    pages: 336,
+    publishDate: new Date(2018, 7, 07),
     likes: 0
 })
+
