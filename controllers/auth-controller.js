628bc4fcc5fd4a4d9a0fc7f4ac218360487b8611
Jordan-Burgess <alfredjordan93@gmail.com>
2022-09-20 18:42:43 -0400

    Add middleware

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -5,4 +5,12 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User")
 const { createUserToken, requireToken } = require("../middleware/auth");
 
+
+router.get('/user/:id', require )
+
+
+
+
+
+
 module.exports = router;
